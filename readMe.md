d3ac21781549321da3c4c79abf4e5231774378d7
khenryjoe <joseph.ketterer@gmail.com>
2020-04-22 16:36:24 +0100

    f

---
+++ readMe.md
@@ -0,0 +1,4 @@
+## React native example of useContext - to create something a bit like redux
+
+This project uses a file called createDataContext to add some functionality
+that makes it a bit more like redux. 
---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,22 @@
+import React, { useContext } from "react";
+import { View, Text, StyleSheet, FlatList , Button} from "react-native";
+import {Context} from "../context/BlogContext";
+
+const IndexScreen = () => {
+  const {state, addBlogPost} = useContext(Context);
+ 
+  return (
+    <View>
+        <Button title ="Add post" onPress={addBlogPost} />
+      <FlatList
+        data={state}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title} </Text>;
+        }}
+      />
+    </View>
+  );
+};
+
+export default IndexScreen;
